389260841b6438ccd0bd70534760f6b83e3acde2
SYD14316 <soporteydesarrollo@umg.edu.mx>
2025-10-24 10:53:29 -0600

    Separacion eh identificacion de secciones

---
+++ public/header.php
@@ -0,0 +1,33 @@
+<header>
+  <!-- CONTACTO Y CUENTA -->
+    <?php //include 'public/top_header.php'; ?>
+  <!-- ENCABEZADO PRINCIPAL -->
+    <div id="header">
+      <div class="container">
+        <div class="row">
+          <!-- LOGO -->
+            <div class="col-md-3">
+              <?php include 'public/logo.php'; ?>
+            </div>
+          <!-- BARRA DE BUSQUEDA -->
+            <div class="col-md-6">
+              <?php //include 'public/search_bar.php'; ?>
+            </div>
+          <!-- CUENTA -->
+            <div class="col-md-3 clearfix">
+              <div class="header-ctn">
+                <!-- WISHLIST -->
+                  <?php //include 'public/wishlist.php'; ?>
+                <!-- CARRITO -->
+                  <?php include 'public/cart.php'; ?>
+                <!-- Menu Toogle -->
+                  <?php include 'public/menu_toggle.php'; ?>
+                <!-- -->
+              </div>
+            </div>
+          <!-- -->
+        </div>
+      </div>
+    </div>
+  <!-- -->
+</header>
